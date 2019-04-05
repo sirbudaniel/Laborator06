75139e81e0d2f76157c0c009aeb65975009cbbfc
Root USO User <student@stud.acs.upb.ro>
2019-04-05 15:20:42 +0300

    implemented tasks

--- labtasks/SingleThreadedServer/app/src/main/java/ro/pub/cs/systems/eim/lab06/singlethreadedserver/network/ServerThread.java
+++ labtasks/SingleThreadedServer/app/src/main/java/ro/pub/cs/systems/eim/lab06/singlethreadedserver/network/ServerThread.java
@@ -54,6 +54,15 @@ public class ServerThread extends Thread {
                 // TODO exercise 5c
                 // simulate the fact the communication routine between the server and the client takes 3 seconds
 
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException interruptedException) {
+                    Log.e(Constants.TAG, interruptedException.getMessage());
+                    if (Constants.DEBUG) {
+                        interruptedException.printStackTrace();
+                    }
+                }
+
                 PrintWriter printWriter = Utilities.getWriter(socket);
                 printWriter.println(serverTextEditText.getText().toString());
                 socket.close();
